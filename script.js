38af68e9472dc13e099407739cb4346a919fa001
joesephorekhov <josephorekhov@gmail.com>
2021-07-25 23:22:08 -0700

    Left, Right Bear Move

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript"; 
-document.querySelector("body").appendChild(h2); 
+document.getElementById(`bear`);
+
+function bearGoLeft(){
+const leftNum = bear.style.left.replace("px", "")
+const left = parseInt(leftNum, 10);
+
+if (left > 0) {
+    bear.style.left = `${left - 3}px`;
+}
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        bearGoLeft();
+    }
+});
+
+document.getElementById(`bear`);
+
+function bearGoRight(){
+    const rightNum = bear.style.left.replace("px", "")
+    const right = parseInt(rightNum, 10);
+    
+if (right > 0) {
+        bear.style.left = `${right + 3}px`;
+}
+}
+    
+document.addEventListener("keydown", function(e) {
+     if (e.key === "ArrowRight") {
+            bearGoRight();
+    }
+});
+    
