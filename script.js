4f3a3eed969d4e177f02bc19c09064d039013ba9
joesephorekhov <josephorekhov@gmail.com>
2021-08-08 19:23:22 -0600

    Bear Finally Moves UP,DOWN

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ function bearGoLeft(){
 const leftNum = bear.style.left.replace("px", "");
 const left = parseInt(leftNum, 10);
 
-if (left > 0) {
+if (left > 10) {
     bear.style.left = `${left - 3}px`;
 }
-}
-
-document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-        bearGoLeft();
-    }
-});
-
-document.getElementById(`bear`);
+}    
 
 function bearGoRight(){
     const rightNum = bear.style.left.replace("px", "");
@@ -25,26 +17,39 @@ if (right > 0) {
         bear.style.left = `${right + 3}px`;
 }
 }
-    
-document.addEventListener("keydown", function(e) {
-     if (e.key === "ArrowRight") {
-            bearGoRight();
-    }
-});
-
-document.getElementById(`bear`);
 
 function bearGoUp(){
-    const upNum = bear.style.top.replace("px", "");
+    const upNum = bear.style.bottom.replace("px", "");
     const up = parseInt(upNum, 10);
     
 if (up > 0) {
-        bear.style.top = `${up + 3}px`;
+        bear.style.bottom = `${up + 3}px`;
 }
 }
+
+function bearGoDown(){
+    const downNum = bear.style.bottom.replace("px", "");
+    const down = parseInt(downNum, 10);
     
-document.addEventListener("keydown", function(e) {
-     if (e.key === "ArrowUp") {
-            bearGoUp();
-    }
-});
+if (down > 0) {
+        bear.style.bottom = `${down - 3}px`;
+}
+}
+
+    document.onkeydown = function(event) {
+        switch (event.keyCode) {
+           case 37:
+                bearGoLeft();
+              break;
+           case 38:
+                bearGoUp();
+              break;
+           case 39:
+                bearGoRight();
+              break;
+           case 40:
+                bearGoDown();
+              break;
+        }
+    };
+
